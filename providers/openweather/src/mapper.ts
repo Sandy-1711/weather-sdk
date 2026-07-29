2fb01425eadb64765b567c02060ec09def5fc284
Sandy-1711 <sandy1711003@gmail.com>
2026-07-29 19:08:48 +0530

    Add OpenWeatherCurrentResponse interface and update OpenWeatherProvider to use structured weather data

--- providers/openweather/src/mapper.ts
+++ providers/openweather/src/mapper.ts
@@ -1,8 +1,45 @@
 import { WeatherResponse } from '@repo/core';
+
+export interface OpenWeatherCurrentResponse {
+    lat: number;
+    lon: number;
+    timezone: string;
+    timezone_offset: number;
+    data: {
+        dt: number;
+        sunrise: number;
+        sunset: number;
+        temp: number;
+        feels_like: number;
+        pressure: number;
+        humidity: number;
+        dew_point: number;
+        clouds: number;
+        uvi: number;
+        visibility: number;
+        wind_speed: number;
+        wind_gust?: number;
+        wind_deg: number;
+        rain?: {
+            '1h': number;
+        };
+        snow?: {
+            '1h': number;
+        };
+        weather: {
+            id: number;
+            main: string;
+            description: string;
+            icon: string;
+        },
+        alerts?: Array<string>;
+    };
+
+}
 export class OpenWeatherMapper {
-    toWeatherResponse(data: any): WeatherResponse {
+    toWeatherResponse(response: OpenWeatherCurrentResponse): WeatherResponse {
         return {
-            temperature: data.main.temp,
+            temperature: response.data.temp,
         };
     }
 }
--- providers/openweather/src/provider.ts
+++ providers/openweather/src/provider.ts
@@ -1,11 +1,44 @@
 import { WeatherProvider, WeatherRequest, WeatherResponse } from '@repo/core';
-import { OpenWeatherMapper } from './mapper';
+import { OpenWeatherCurrentResponse, OpenWeatherMapper } from './mapper';
 export class OpenWeatherProvider implements WeatherProvider {
     readonly name = 'OpenWeather';
     private readonly mapper = new OpenWeatherMapper();
 
     async current(request: WeatherRequest): Promise<WeatherResponse> {
-        const weatherData = { main: { temp: 25 } };
+        const weatherData: OpenWeatherCurrentResponse = {
+            "lat": 51.5,
+            "lon": -0.1,
+            "timezone": "Europe/London",
+            "timezone_offset": 3600,
+            "data":
+            {
+                "dt": 1777449371,
+                "sunrise": 1777437375,
+                "sunset": 1777490344,
+                "temp": 286.42,
+                "feels_like": 285.32,
+                "pressure": 1024,
+                "humidity": 58,
+                "dew_point": 278.34,
+                "uvi": 1.55,
+                "clouds": 0,
+                "visibility": 10000,
+                "wind_speed": 8.23,
+                "wind_deg": 70,
+                "weather": {
+                    "id": 800,
+                    "main": "Clear",
+                    "description": "sky is clear",
+                    "icon": "01d"
+                }
+                ,
+                "alerts": [
+                    "8B46C632-DCA7-44D7-8BDF-02445621BAFF",
+                    "29F58A35-BB91-4A73-9F46-9FC64BDF604F",
+                ]
+            }
+
+        }
         return this.mapper.toWeatherResponse(weatherData);
     }
 }
